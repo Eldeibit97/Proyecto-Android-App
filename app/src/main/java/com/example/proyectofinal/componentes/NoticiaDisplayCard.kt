b577896bdb12444103c90b1974436617c357b71a
Eldeibit97 <david.mtz.rscn@gmail.com>
2025-10-22 18:45:44 -0600

    fix de estilo de noticia

--- app/src/main/java/com/example/proyectofinal/componentes/NoticiaDisplayCard.kt
+++ app/src/main/java/com/example/proyectofinal/componentes/NoticiaDisplayCard.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -101,6 +102,8 @@ fun NoticiaDisplayCard(
                     modifier = Modifier.fillMaxWidth())
                 Spacer(modifier = Modifier.padding(6.dp))
                 AnimatedVisibility(visible = visible) {
+                    HorizontalDivider(modifier = Modifier.fillMaxWidth())
+                    Spacer(modifier = Modifier.padding(6.dp))
                     Text(
                         text = noticia.cuerpo,
                         textAlign = TextAlign.Justify,
